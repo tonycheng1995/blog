9299171fbfec5539fef0bab6615aa026616d328c
tonycheng1995 <tonycheng1995@foxmail.com>
2022-03-13 20:42:58 +0800

    fix gitalk

--- assets/js/search.js
+++ assets/js/search.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     var sjs = SimpleJekyllSearch({
         searchInput: document.getElementById('search-input'),
         resultsContainer: document.getElementById('results-container'),
-        json: '/taiji/search.json'
+        json: '/search.json'
     });
 });
 
